cce6d29543cb9e04c24d78001eaf5070da11abf6
azhyrkevich <anastasiya-zhyrkevich@yandex.ru>
2019-02-22 00:10:18 +0300

    need to fix gt lt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from copy import deepcopy as copy
+
 
 n, m = None, None
 l, r = None, None
@@ -26,15 +28,18 @@ class Solution(object):
         self.slices = slices
 
     def print_out(self, filename):
-        with open(filename) as f:
+        with open(filename, 'w') as f:
             f.write("%d\n" % len(self.slices))
             for sl in self.slices:
                 for c in sl:
                     f.write("%d " % (c - 1))
                 f.write("\n")
 
-    def __le__(self, other):
-        return (self.reward - self.other)
+    def __lt__(self, other):
+        return (- self.reward + other.reward)
+
+    def __gt__(self, other):
+        return (self.reward - other.reward)
 
     def add(self, r, sl):
         self.reward += r
@@ -43,8 +48,14 @@ class Solution(object):
     def add_slices(self, sl):
         for s in sl:
             self.reward += slice_size(*s)
+
         self.slices.extend(sl)
 
+    def __str__(self):
+        return "S: %d, %s" % (self.reward, self.slices)
+
+    def __repr__(self):
+        return "S: %d, %s" % (self.reward, self.slices)
 
 read_file(PATH)
 
@@ -90,23 +101,24 @@ def get_ingr_cnt(dp, i1, i2, j1, j2):
     return dp[i2][j2] - dp[i1 - 1][j2] - dp[i2][j1 - 1] + dp[i1 - 1][j1 - 1]
 
 def v_stripe_dp(dp, i1, i2, j1, j2):
-    print "v_stripe_dp %d %d %d %d" % (i1, i2, j1, j2)
+    # print "v_stripe_dp %d %d %d %d" % (i1, i2, j1, j2)
     assert(j2 - j1 <= 1)
 
     bs = [Solution(0, [])] * (i2 + 1)
     for i in xrange(i1, i2 + 1):
-        bs[i] = bs[i - 1]
+        bs[i] = copy(bs[i - 1])
 
         for ii in xrange(i, i1 - 1, -1):
             if is_slice(dp, ii, i, j1, j2):
 
-                pot_sl = bs[ii - 1].copy()
+                pot_sl = copy(bs[ii - 1])
                 pot_sl.add_slices([(ii, i, j1, j2)])
 
-                bs[i] = max(bs[i], pot_sl)
+                if pot_sl.reward > bs[i].reward:
+                    bs[i] = pot_sl
 
-    print "v_stripe_dp %d %d %d %d" % (i1, i2, j1, j2)
-    print bs
+    # print "v_stripe_dp %d %d %d %d" % (i1, i2, j1, j2)
+    # print bs
     return bs[i2]
 
 
@@ -115,25 +127,28 @@ def vertical_dp(pizza):
 
     dp = get_dp(pizza, n, m)
 
-    best_solution = [0] * (m + 1)
+    best_solution = [Solution(0, [])] * (m + 1)
 
     for j in xrange(1, m + 1):
-        best_solution[j] = best_solution[j - 1]
+        best_solution[j] = copy(best_solution[j - 1])
         print "j ", j
         for d in xrange(1, min(j, 2) + 1):
 
-            pot_sol = v_stripe_dp(dp, 1, n, j - d + 1, j)
-            pot_sol += best_solution[j - d]
+            pot_sol = copy(best_solution[j - d])
+            pot_sol.add_slices(v_stripe_dp(dp, 1, n, j - d + 1, j).slices)
 
-            best_solution[j] = max(
-                best_solution[j],
-                pot_sol
-            )
+            if pot_sol.reward > best_solution[j].reward:
+                best_solution[j] = pot_sol
+
+        print best_solution[j]
+
     print best_solution
+    return best_solution[m]
 
 
 
-vertical_dp(pizza)
+t1 = vertical_dp(pizza)
+t1.print_out('a.out')
 
 
 # dp = get_dp(pizza, n, m)
